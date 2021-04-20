f2291f026b3f02ae699d63c1c86f699f809cd2d6
simak95 <sk724@njit.edu>
2021-04-20 14:07:48 -0400

    Task: updated app.py

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 import mysql.connector
 import simplejson as json
 from flask import Flask, Response
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -25,6 +26,12 @@ def cities_import() -> List[Dict]:
 
     return result
 
+@app.route('/')
+def index():
+    user = {'username': 'Miguel'}
+    cities_data = cities_import()
+
+    return render_template('index.html', title='Home', user=user, cities=cities_data)
 
 @app.route('/')
 def index() -> str:

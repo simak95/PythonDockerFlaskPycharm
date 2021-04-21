90dcb7eb15e6cc204453590e17ac433776be925b
simak95 <sk724@njit.edu>
2021-04-21 13:53:39 -0400

    Task: added sqltable.png

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import simplejson as json
 from flask import Flask, Response
 from flask import render_template
 
+
 app = Flask(__name__)
 
 
@@ -26,15 +27,17 @@ def cities_import() -> List[Dict]:
 
     return result
 
+
 @app.route('/')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Sima'}
     cities_data = cities_import()
 
     return render_template('index.html', title='Home', user=user, cities=cities_data)
 
-@app.route('/')
-def index() -> str:
+
+@app.route('/api/cities')
+def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
@@ -42,5 +45,3 @@ def index() -> str:
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
-

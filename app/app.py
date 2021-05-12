9585940c525432837e7bca870656f90429cf48b7
simak95 <sk724@njit.edu>
2021-05-12 19:12:04 -0400

    Task: update README.MD

--- app/app.py
+++ app/app.py
@@ -114,9 +114,7 @@ def api_edit(name) -> str:
 
 @app.route('/api/v1/Names', methods=['POST'])
 def api_add() -> str:
-
     content = request.json
-
     cursor = mysql.get_db().cursor()
     inputData = (content['index'], content['year'], content['age'],
                  content['name'], content['movie'])

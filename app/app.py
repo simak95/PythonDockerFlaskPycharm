c1048280a43caaa8533bdd4c8f81c62dba1da8c2
simak95 <sk724@njit.edu>
2021-05-15 01:13:29 -0400

    Task: added new init.sql and files

--- app/app.py
+++ app/app.py
@@ -18,36 +18,35 @@ mysql.init_app(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Oscar Winners Female'}
+    user = {'username': 'Sima Oscar Winners'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM oscarfemale')
+    cursor.execute('SELECT * FROM male')
     result = cursor.fetchall()
     return render_template('index.html', title='Home', user=user, Names=result)
 
 
-@app.route('/view/<int:index>', methods=['GET'])
-def record_view(index):
+@app.route('/view/<string:Name>', methods=['GET'])
+def record_view(Name):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM oscarfemale WHERE `index`=%s', index)
+    cursor.execute('SELECT * FROM male WHERE Name=%s', Name)
     result = cursor.fetchall()
     return render_template('view.html', title='View Form', award=result[0])
 
 
-@app.route('/edit/<int:index>', methods=['GET'])
-def form_edit_get(index):
+@app.route('/edit/<string:Name>', methods=['GET'])
+def form_edit_get(Name):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM oscarfemale WHERE `index`=%s', index)
+    cursor.execute('SELECT * FROM male WHERE Name=%s', Name)
     result = cursor.fetchall()
     return render_template('edit.html', title='Edit Form', award=result[0])
 
 
-@app.route('/edit/<int:index>', methods=['POST'])
-def form_update_post(index):
+@app.route('/edit/<string:Name>', methods=['POST'])
+def form_update_post(Name):
     cursor = mysql.get_db().cursor()
     inputData = (request.form.get('index'), request.form.get('year'), request.form.get('age'),
-                 request.form.get('name'), request.form.get('movie'), index)
-    sql_update_query = """UPDATE oscarfemale t SET t.index = %s, t.year = %s, t.age = %s, t.name = %s, 
-    t.movie = %s WHERE t.index = %s"""
+                 request.form.get('name'), request.form.get('movie'), Name)
+    sql_update_query = """UPDATE male t SET t.index = %s, t.year = %s, t.age = %s, t.name = %s, t.movie = %s WHERE t.name = %s"""
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
@@ -63,17 +62,17 @@ def form_insert_post():
     cursor = mysql.get_db().cursor()
     inputData = (request.form.get('index'), request.form.get('year'), request.form.get('age'),
                  request.form.get('name'), request.form.get('movie'))
-    sql_insert_query = """INSERT INTO oscarfemale (index,year,age,name,movie) VALUES (%s, %s,%s, %s,%s)"""
+    sql_insert_query = """INSERT INTO male (index,year,age,name,movie) VALUES (%s, %s,%s, %s,%s)"""
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/delete/<int:index>', methods=['POST'])
-def form_delete_post(index):
+@app.route('/delete/<string:Name>', methods=['POST'])
+def form_delete_post(Name):
     cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM oscarfemale WHERE `index` = %s """
-    cursor.execute(sql_delete_query, index)
+    sql_delete_query = """DELETE FROM male WHERE name = %s """
+    cursor.execute(sql_delete_query, Name)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
@@ -81,61 +80,40 @@ def form_delete_post(index):
 @app.route('/api/v1/Names', methods=['GET'])
 def api_browse() -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM oscarfemale')
+    cursor.execute('SELECT * FROM male')
     result = cursor.fetchall()
-    json_result = json.dumps(result)
+    json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/Names/<int:index>', methods=['GET'])
-def api_retrieve(index) -> str:
+@app.route('/api/v1/Names/<int:Index>', methods=['GET'])
+def api_retrieve(Index) -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM oscarfemale WHERE `index`=%s', index)
+    cursor.execute('SELECT * FROM male WHERE `index`=%s', Index)
     result = cursor.fetchall()
-    json_result = json.dumps(result)
+    json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/Names/<string:name>', methods=['PUT'])
-def api_edit(name) -> str:
-    cursor = mysql.get_db().cursor()
-    content = request.json
-    inputData = (content['index'], content['year'], content['age'],
-                 content['name'], content['movie'], name)
-    sql_update_query = """UPDATE oscarfemale t SET t.index = %s, t.year = %s, t.age = %s, t.name = 
-            %s, t.movie = %s WHERE t.name = %s """
-    cursor.execute(sql_update_query, inputData)
-    mysql.get_db().commit()
+@app.route('/api/v1/Names/<string:Name>', methods=['POST'])
+def api_add(Name) -> str:
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/Names', methods=['POST'])
-def api_add() -> str:
-    content = request.json
-    cursor = mysql.get_db().cursor()
-    inputData = (content['index'], content['year'], content['age'],
-                 content['name'], content['movie'])
-    sql_insert_query = """INSERT INTO oscarfemale(`index`,year,age,name,movie) VALUES (%s, %s,%s, %s,%s) """
-    cursor.execute(sql_insert_query, inputData)
-    mysql.get_db().commit()
+@app.route('/api/v1/Names/<string:Name>', methods=['PUT'])
+def api_edit(Name) -> str:
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/Names/<string:name>', methods=['DELETE'])
-def api_delete(name) -> str:
-    cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM oscarfemale WHERE name = %s """
-    cursor.execute(sql_delete_query, name)
-    mysql.get_db().commit()
-    resp = Response(status=200, mimetype='application/json')
+@app.route('/api/Names/<string:Name>', methods=['DELETE'])
+def api_delete(Name) -> str:
+    resp = Response(status=210, mimetype='application/json')
     return resp
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
-
-
